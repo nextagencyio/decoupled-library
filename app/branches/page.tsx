0ca9793dc0d173743c4977e25ddec5663cad75a4
Jay Callicott <jrcallicott@gmail.com>
2026-03-08 08:42:46 -0500

    fix: mark header-dependent routes as dynamic

--- app/branches/page.tsx
+++ app/branches/page.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header'
 import BranchCard from '../components/BranchCard'
 
 export const revalidate = 3600
+export const dynamic = 'force-dynamic'
 
 export const metadata: Metadata = {
   title: 'Branch Locations | Riverside Public Library',
